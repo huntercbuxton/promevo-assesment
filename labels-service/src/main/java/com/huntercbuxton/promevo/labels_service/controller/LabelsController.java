ef9332d1bb70086762cf5af9ca04a793c360e8e6
Hunter Buxton <hunter.buxton@protonmail.com>
2026-06-20 14:15:36 -0600

    add google oauth example

--- labels-service/src/main/java/com/huntercbuxton/promevo/labels_service/controller/LabelsController.java
+++ labels-service/src/main/java/com/huntercbuxton/promevo/labels_service/controller/LabelsController.java
@@ -9,13 +9,46 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.api.services.gmail.Gmail;
+import com.google.api.services.gmail.model.ListMessagesResponse;
+import com.huntercbuxton.promevo.labels_service.GmailConfig;
 import com.huntercbuxton.promevo.labels_service.model.GmailLabel;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
+@Slf4j 
 public class LabelsController {
 
+	private final GmailConfig gmailConfig;
+
+	public LabelsController(GmailConfig gmailConfig) {
+		this.gmailConfig = gmailConfig;
+	}
+	
+	@GetMapping("/api/messages")
+    public String listMessages() {
+		log.info("called /api/messages endpoint handler");
+		
+        try {
+            Gmail service = gmailConfig.getGmailService();
+            // Fetch messages from the authenticated user's profile
+            ListMessagesResponse response = service.users().messages().list("me").setMaxResults(5L).execute();
+            
+            if (response.getMessages() == null || response.getMessages().isEmpty()) {
+                return "No messages found.";
+            }
+            return response.getMessages().toString();
+        } catch (Exception e) {
+            return "Error retrieving messages: " + e.getMessage();
+        }
+    }
+	
+	    
 	@GetMapping("/{id}")
 	public GmailLabel getLabel(@PathVariable("id") String id) {
+		log.info("called get /{id} endpoint handler");
+		
 		GmailLabel resp = new GmailLabel();
 		return resp;
 	}
@@ -23,19 +56,23 @@ public class LabelsController {
 	
 	@DeleteMapping("/{id}")
 	public String deleteLabel(@PathVariable("id") String id) {
+		log.info("called delete /{id} endpoint handler");
 		return "endpoint not yet implemented";
 	}
 	
 	
 	@GetMapping("/list")
 	public List<GmailLabel> listLabels() {
+		log.info("called get /list endpoint handler");
 		List<GmailLabel> resp = new ArrayList<GmailLabel>();
 		return resp;
 	}
 	
 	
+	
 	@PostMapping("/create")
 	public GmailLabel createLabel() {
+		log.info("called post /create endpoint handler");
 		GmailLabel resp = new GmailLabel();
 		return resp;
 	}
@@ -43,6 +80,7 @@ public class LabelsController {
 	
 	@PostMapping("/update")
 	public GmailLabel updateLabel() {
+		log.info("called post /update endpoint handler");
 		GmailLabel resp = new GmailLabel();
 		return resp;
 	}
