99fbffe0532b5f748c1cb945b1ef45acf8d25ba1
Hunter Buxton <hunter.buxton@protonmail.com>
2026-06-20 18:39:19 -0600

    route placeholders

--- labels-ui/src/main.tsx
+++ labels-ui/src/main.tsx
@@ -1,5 +1,3 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 // mui dependencies
@@ -7,11 +5,54 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import ReactDOM from "react-dom/client"; 
 
+const ListLabels = function() {
+  return <h1>List Labels Page</h1>
+}
+const ViewLabel = function() {
+  return <h1>View Label Page</h1>
+}
+const LandingPage = () => <h1>Landing Page</h1>
+const UpdatePage = () => <h1>Update Page</h1>
+const CreatePage = () => <h1>Create Page</h1>
+const DeletePage = () => <h1>Delete Page</h1>
 
+const router = createBrowserRouter([
+  {
+    path: "/", 
+    Component: App,
+    children: [
+      { index: true, Component: LandingPage },
+      {
+        path: "list",
+        Component: ListLabels
+      },
+      //https://reactrouter.com/start/data/routing#dynamic-segments
+      {
+        path: "labels/:id",
+        Component: ViewLabel
+      },
+      {
+        path: "create",
+        Component: CreatePage
+      },
+      {
+        path: "update",
+        Component: UpdatePage
+      },
+      {
+        path: "delete",
+        Component: DeletePage
+      }
+    ]
+  },
+]);
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+  <RouterProvider router={router} />,
+);
